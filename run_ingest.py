a43ea70b2bc8696170d0c5cb8eb7169ac589ac22
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-15 23:42:26 +0200

    updated feed of database using langchain

--- run_ingest.py
+++ run_ingest.py
@@ -1,3 +1,4 @@
+
 import json
 from dotenv import load_dotenv
 from src.crawler import fetch_live_news
@@ -6,40 +7,37 @@ from src.database import Neo4jManager
 
 load_dotenv()
 
+
 def main():
-    articles = fetch_live_news(limit=200)
+    articles = fetch_live_news()
+
     if not articles:
         return
-
+    
     extractor = GroqExtractor()
     db_manager = Neo4jManager()
-    json_backup_data = []
 
     for i, article in enumerate(articles, 1):
-        print(f"\n--- Processing Article {i}/{len(articles)}: {article['title']} ---")
-        graph_data = extractor.extract(article['content'])
-        
-        if graph_data:
-            print(f"Extracted {len(graph_data.get('entities', []))} entities.")
-            graph_data['metadata'] = {
-                "title": article['title'],
-                "link": article['link'],
-                "published": article['published']
-            }
-            
-            if len(json_backup_data) < 15:
-                json_backup_data.append(graph_data)
+        print(f"processing article {i} / {len(articles)} : {article['title']}")
 
-            db_manager.ingest_graph(graph_data)
+        metadata = {
+            "title": article['title'],
+            "link": article['link'],
+            "published": article['published']
+        }
+
+        graph_documents = extractor.extract(article['content'], metadata)
+
+        if graph_documents:
+            nodes_count = len(graph_documents[0].nodes)
+            edges_count = len(graph_documents[0].relationships)
+            print(f"extracted {nodes_count} nodes and {edges_count} edges")
+
+            db_manager.ingest_graph_documents(graph_documents)
+
         else:
-            print("Failed to extract data.")
+            print("failed to extract data")
 
-    if json_backup_data:
-        with open("live_ingestion_results.json", "w") as f:
-            json.dump(json_backup_data, f, indent=2)
-        print(f"\nBackup saved. Finished processing {len(articles)} articles.")
-
-    db_manager.close()
 
 if __name__ == "__main__":
     main()
